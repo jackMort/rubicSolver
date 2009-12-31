d2984089550974a09466c214ae07b6728dc9c570
Lech Twaróg - Jack <lech.twarog@gmail.com>
2009-12-31 18:14:12 +0100

    added getopt.h

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 #include "rubicSolver.h"
 
+void usage()
+{
+	printf( "rubicSolver ...\n" );
+}
+//----
+
 CUBE cube;
+struct option long_options[] = {
+	{ "help", no_argument, 0, 'h' }
+};
+
 int main( int argc, char *argv[] ) 
 {
+	int c, i;
+	while( ( c = getopt_long( argc, argv, "h", long_options, &i) ) != -1 )
+	{
+		switch( c )
+		{
+			default:
+				usage(); exit( 0 );
+		}
+	}
+
 	// init new cube
 	init_cube( &cube, 3 );
 	// print cube
@@ -15,6 +37,5 @@ int main( int argc, char *argv[] )
 	
 	//..
 	return 0;
-
-
+}
 // vim: fdm=marker ts=4 sts=4 sw=4
